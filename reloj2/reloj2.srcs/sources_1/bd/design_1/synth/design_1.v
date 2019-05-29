a23366527cbf428bf3eeb2cade1d630d2a17bd65
root <jhosshua_1995@hotmail.com>
2019-05-29 00:01:01 -0500

    first commit

---
+++ reloj2/reloj2.srcs/sources_1/bd/design_1/synth/design_1.v
@@ -0,0 +1,51 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (lin64) Build 2405991 Thu Dec  6 23:36:41 MST 2018
+//Date        : Sun Apr  7 03:15:35 2019
+//Host        : jose-vpceg20el running 64-bit Ubuntu 16.04.6 LTS
+//Command     : generate_target design_1.bd
+//Design      : design_1
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=2,numReposBlks=2,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=1,numPkgbdBlks=0,bdsource=USER,da_board_cnt=1,da_clkrst_cnt=1,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "design_1.hwdef" *) 
+module design_1
+   (a,
+    b,
+    clk,
+    led,
+    led2,
+    reset);
+  input a;
+  input b;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK, CLK_DOMAIN design_1_clk, FREQ_HZ 100000000, INSERT_VIP 0, PHASE 0.000" *) input clk;
+  output led;
+  output led2;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 RST.RESET RST" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME RST.RESET, INSERT_VIP 0, POLARITY ACTIVE_HIGH" *) input reset;
+
+  wire a_0_1;
+  wire b_0_1;
+  wire clk_in1_0_1;
+  wire clk_wiz_clk_out1;
+  wire reloj_0_0_led2;
+  wire reloj_0_led;
+  wire reset_1;
+
+  assign a_0_1 = a;
+  assign b_0_1 = b;
+  assign clk_in1_0_1 = clk;
+  assign led = reloj_0_led;
+  assign led2 = reloj_0_0_led2;
+  assign reset_1 = reset;
+  design_1_clk_wiz_0 clk_wiz
+       (.clk_in1(clk_in1_0_1),
+        .clk_out1(clk_wiz_clk_out1),
+        .reset(reset_1));
+  design_1_reloj_0_0 reloj_0_0
+       (.a(a_0_1),
+        .b(b_0_1),
+        .clk(clk_wiz_clk_out1),
+        .led(reloj_0_led),
+        .led2(reloj_0_0_led2));
+endmodule
